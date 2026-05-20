cf0ab40f228ecaec717dea96cd92e7adf02ec28a
Sam Phillips <abjas.phillips@gmail.com>
2026-05-20 09:34:21 -0500

    No Custom url now

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+
+// GitHub Pages project site: https://<user>.github.io/Trip-Mapper-from-Excel/
+export default defineConfig({
+  base: "/Trip-Mapper-from-Excel/",
+});
